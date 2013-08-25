fde6e4c07fc6871cb0f0ed90a6f4eff6dd34cf8c
Mirko Kiefer <mail@mirkokiefer.com>
2013-08-25 17:26:53 +0200

    basic read/write

---
+++ test/index.js
@@ -0,0 +1,29 @@
+
+var assert = require('assert')
+var histo = require('../lib/index')
+
+var db = histo.createDB()
+
+var commits = [
+  {data: '123'},
+  {data: '456'},
+  {data: '789'}
+]
+
+describe('HistoDB', function() {
+  it('should write some data', function(done) {
+    var commit = commits[0]
+    db.write(commit.data, function(err, res) {
+      assert.ok(res.head)
+      commit.hash = res.head
+      done()
+    })
+  })
+  it('should read the data', function(done) {
+    var commit = commits[0]
+    db.read(commit.hash, function(err, res) {
+      assert.equal(res, commit.data)
+      done()
+    })
+  })
+})
