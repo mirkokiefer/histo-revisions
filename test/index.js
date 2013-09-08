a4baf7ca1279990df4d06d60640ba80ba993faf8
Mirko Kiefer <mail@mirkokiefer.com>
2013-09-08 12:16:40 +0100

    change naming

--- test/index.js
+++ test/index.js
@@ -30,7 +30,7 @@ var forkBHead = revs2[2]
 var writeRevs = function(db, revs, cb) {
   async.forEach(revs, function(each, cb) {
     db.put(each.data, function(err, res) {
-      each.hash = res.head
+      each.ref = res.head
       cb()
     })
   }, cb)
@@ -45,13 +45,13 @@ describe('HistoDB', function() {
       var rev = revs1[0]
       db.put(rev.data, function(err, res) {
         assert.ok(res.head)
-        rev.hash = res.head
+        rev.ref = res.head
         done()
       })
     })
     it('should read the data', function(done) {
       var rev = revs1[0]
-      db.read(function(err, res) {
+      db.get(function(err, res) {
         assert.equal(res, rev.data)
         done()
       })
@@ -61,20 +61,20 @@ describe('HistoDB', function() {
       writeRevs(db, revs, done)
     })
     it('should read the head', function(done) {
-      db.read(function(err, res) {
+      db.get(function(err, res) {
         assert.equal(res, revs1[5].data)
         done()
       })
     })
     it('should read the data of a previous revision', function(done) {
-      db.readAtRevision(revs1[0].hash, function(err, res) {
+      db.getAtRef(revs1[0].ref, function(err, res) {
         assert.deepEqual(res, revs1[0].data)
         done()
       })
     })
     it('should set the head to a previous rev leaving a fork A', function(done) {
-      db.setHead(commonAncestor.hash, function() {
-        db.read(function(err, res) {
+      db.setHead(commonAncestor.ref, function() {
+        db.get(function(err, res) {
           assert.equal(res, commonAncestor.data)
           done()
         })
@@ -83,31 +83,30 @@ describe('HistoDB', function() {
     it('should write more data creating a fork B', function(done) {
       writeRevs(db, revs2, done)
     })
-    var revs
+    var refDiff
     var expectedRevs = revs1.slice(3)
-    it('should find the missing revs to get from fork B to fork A', function(done) {
-      db.revisionHashDifference(db.head(), forkAHead.hash, function(err, res) {
-        var expectedDiff = expectedRevs.map(function(each) { return each.hash }).reverse()
+    it('should find the missing refs to get from fork B to fork A', function(done) {
+      db.refDifference(db.head(), forkAHead.ref, function(err, res) {
+        var expectedDiff = expectedRevs.map(function(each) { return each.ref }).reverse()
         assert.deepEqual(res, expectedDiff)
-        revs = res
+        refDiff = res
         done()
       })
     })
-    it('should create an iterator for bulk-reading rev data', function(done) {
-      var revIterator = db.readRevisions(revs)
+    it('should create an iterator for bulk-reading revisions', function(done) {
+      var revIterator = db.readRevisions(refDiff)
       iterators.toArray(revIterator, function(err, res) {
         res.reverse().map(function(each, i) {
           assert.equal(each.data, expectedRevs[i].data)
-          assert.equal(each.ancestors[0], revs1[i+2].hash)
+          assert.equal(each.ancestors[0], revs1[i+2].ref)
         })
         done()
       })
     })
     it('should find all revs of fork A', function(done) {
-      db.revisionHashDifference(null, forkAHead.hash, function(err, res) {
-        var expectedDiff = revs1.map(function(each) { return each.hash }).reverse()
+      db.refDifference(null, forkAHead.ref, function(err, res) {
+        var expectedDiff = revs1.map(function(each) { return each.ref }).reverse()
         assert.deepEqual(res, expectedDiff)
-        revs = res
         done()
       })
     })
